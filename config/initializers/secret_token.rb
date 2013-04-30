ec0e6f0b14f2481847bd7d52b624210728c64886
jessicalouise <jstewart@smartling.com>
2013-04-30 16:16:47 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThreeJobs::Application.config.secret_token = 'f932da0cf6583ec2afc61e1d4c31f0bc3f8dbba04d7b149860c898472805a61cfbdc13b2011edca36d12ad0c80d40c1b839304383263aab3288cc7a3eb8e37c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ThreeJobs::Application.config.session_store :cookie_store, key: '_three_jobs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ThreeJobs::Application.config.session_store :active_record_store
